fd7843deaa5cf79feef83f7823769e6964a7a9c9
Thomas Röggla <t.roggla@cwi.nl>
2022-09-21 19:31:24 +0200

    updated eslint config and removed unused packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,5 @@
 module.exports = {
   "env": {
-    "browser": true,
     "es6": true,
     "node": true
   },
@@ -14,30 +13,16 @@ module.exports = {
   ],
   "extends": [
     "eslint:recommended",
-    "plugin:react/recommended",
     "plugin:@typescript-eslint/eslint-recommended"
   ],
-  "globals": {
-    "Atomics": "readonly",
-    "SharedArrayBuffer": "readonly"
-  },
   "parser": "@typescript-eslint/parser",
   "parserOptions": {
-    "ecmaFeatures": {
-      "jsx": true
-    },
     "ecmaVersion": 2018,
     "sourceType": "module"
   },
   "plugins": [
-    "react",
     "@typescript-eslint"
   ],
-  "settings": {
-    "react": {
-      "version": "detect"
-    }
-  },
   "rules": {
     "indent": [
       "error",
