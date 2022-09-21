1bd10427c8ee409ce587f20d1eda0043c3b80105
Thomas Röggla <t.roggla@cwi.nl>
2022-09-21 18:38:26 +0200

    added script for adding new user accounts

---
+++ register.js
@@ -0,0 +1,43 @@
+const crypto = require("crypto");
+const prompt = require("prompt");
+const sqlite = require("sqlite3").verbose();
+const uuid = require("uuid");
+
+const db = new sqlite.Database("./db/database.sqlite", sqlite.OPEN_READWRITE, (err) => {
+  if (err) {
+    console.log("Database connection failed:", err);
+    process.exit(1);
+  }
+});
+
+prompt.start();
+
+prompt.get([{ name: "username" }, { name: "password", hidden: true }], (err, result) => {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  const { username, password } = result;
+
+  const salt = crypto.randomBytes(16).toString("hex");
+  const hashedPassword = crypto.pbkdf2Sync(
+    password, salt,
+    10000, 512, "sha512"
+  ).toString("hex");
+
+  db.run(
+    "INSERT INTO users (id, username, password, salt, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?)",
+    [uuid.v4(), username, hashedPassword, salt, new Date().toISOString(), new Date().toISOString()],
+    (err) => {
+      if (err) {
+        console.log("Could not insert user:", err);
+        process.exit(1);
+      }
+
+      console.log(`User '${username}' created successfully!`);
+    }
+  );
+
+  db.close();
+});
