3c3bfb5b63dbad0facb0e9eed8f0cc4180d03385
Thomas Röggla <t.roggla@cwi.nl>
2022-09-21 17:23:19 +0200

    removed unused authRequired middleware function

--- util/middleware.ts
+++ util/middleware.ts
@@ -11,21 +11,3 @@ import passport from "passport";
 export function tokenRequired(req: Request, res: Response, next: NextFunction): RequestHandler {
   return passport.authenticate("jwt", { session: false })(req, res, next);
 }
-
-/**
- * Middleware function which makes routes which it is applied to require a
- * an active session, achieved through a session cookie.
- *
- * @returns a JWT request handler which can be used as middleware function
- */
-export function authRequired(req: Request, res: Response, next: NextFunction) {
-  if (req.user) {
-    next();
-  } else {
-    res.status(401);
-    res.send({
-      status: "ERR",
-      message: "Authorisation required"
-    });
-  }
-}
