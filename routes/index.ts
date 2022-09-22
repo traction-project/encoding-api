4f0d76a26953d6488a6d569db8c1723aa5398e05
Thomas Röggla <t.roggla@cwi.nl>
2022-09-22 18:40:45 +0200

    added documentation for all API routes

--- routes/index.ts
+++ routes/index.ts
@@ -6,6 +6,11 @@ import APIRouter from "./api";
 const router = Router();
 router.use("/api", APIRouter);
 
+/**
+ * Returns the contents of the file REVISION in order to check the current
+ * version of the API. If no such file is found, the string `unknown` is
+ * returned.
+ */
 router.get("/revision", (_, res) => {
   try {
     const REVISION = readFileSync("REVISION").toString().trim();
